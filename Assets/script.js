4d525cf364bbc9e05293e20b4b9096eb70ca5bce
Christopher Ponzio <chrisponz11@yahoo.com>
2022-01-17 17:52:18 -0500

    Wireframe for Javascript

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+// Main vars
+var timekeeper = document.querySelector("nav");
+var startbutton = document.querySelector("#button");
+var question = [""];
+var answers = [""];
+var secondsleft = 10;
+
+
+// Button
+startbutton.addEventListener("click", quizstart);
+
+//Runs the quiz
+function quizstart() {
+    // add timer start function
+    //add question function
+    //add game end
+}
+
+//timer function
+    //x time
+    //subtract for worng answers
+    //end of timer end quiz alert
+
+//question function
+    // 5 questions
+    // record answers
+    //end quiz alert
+    // alert correct and incorrect 
+
+//game end
+    //show scores
+    //highscores record
