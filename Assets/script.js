93e0a2a4228d2f8c7eae26420c8e39be7b74f767
Christopher Ponzio <chrisponz11@yahoo.com>
2022-01-23 09:56:03 -0500

    Rework of project.

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,17 @@
 // Main vars
 var timekeeper = document.querySelector("nav");
-var startbutton = document.querySelector("#button");
-var question = [""];
-var answers = [""];
+var startbutton = document.querySelector("#startbutton");
+var questionEl = document.querySelector("#question");
+var answerEl = document.querySelector("#answer");
 var timeLeft = 78;
-//Timer
-function countdown() {
-    vartimeInterval = setInterval(function () {
-        if (timeLeft >1) {
-            timekeeper.textContent = timeLeft + 'seconds remaining';
-            timeLeft -- ;
-        } else {
-            timekeeper.textContent = 'GAME OVER';
-            clearInterval(timeInterval);
-        }
-    },1000);
-}
 
+var questionIndex = [q1, q2, q3, q4, q5];
+var q1 = new Question("Arrays in Javascript can be used to store __.", ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],3);
+var q2 = new Question("A very useful tool used during development and debugging for printing content to the debugger is?", ["Javascript","Terminal/Bash", "For Loops", "Console Log"],3);
+var q3 = new Question("String values must be enclosed within___ when being assigned to variables.", ["Commas", "Curly Brackets", "Quotes", "Parentheses"],2);
+var q4 = new Question("Commonly used data types DO NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"], 2);
+var q5 = new Question("The condition in an if / else statement is enclosed within__.", ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"], 2);
+let currentQuestion = 0;
 
 // Button
 startbutton.addEventListener("click", quizstart);
@@ -24,17 +19,31 @@ startbutton.addEventListener("click", quizstart);
 //Runs the quiz
 function quizstart() {
     countdown();
-    
     //add question function
     //add game end
 }
 
 //timer function
-    //x time
+function countdown() {
+    var timeInterval = setInterval(function () {
+        if (timeLeft >1) {
+            timekeeper.textContent = timeLeft + 'seconds remaining';
+            timeLeft -- ;
+        } else if (timeLeft === 1) {
+            timekeeper.textContent = timeLeft + ' second remaining';
+             timeLeft--;
+        } else {
+            timekeeper.textContent = 'GAME OVER';
+            clearInterval(timeInterval);
+            window.alert("GAME OVER!!");
+        }
+    }, 1000);
+}
     //subtract for worng answers
-    //end of timer end quiz alert
+
 
 //question function
+
     // 5 questions
     // record answers
     //end quiz alert
